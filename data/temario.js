1a0c7b6f58d6482c73d0fe372261a34dfb58c750
xboxseriespass20-cloud <xboxseriespass2.0@gmail.com>
2026-03-12 01:29:45 -0600

    Add files via upload

---
+++ data/temario.js
@@ -0,0 +1,33 @@
+
+        const topicsData = {
+            matematicas: [
+                "Números enteros y fracciones",
+                "Álgebra básica",
+                "Geometría plana",
+                "Problemas de razonamiento lógico"
+            ],
+            espanol: [
+                "Ortografía y puntuación",
+                "Comprensión lectora",
+                "Redacción de textos",
+                "Análisis de literatura"
+            ],
+            ciencias: [
+                "Cuerpo humano y biología",
+                "Física básica: fuerza y movimiento",
+                "Química elemental",
+                "Medio ambiente"
+            ],
+            historia: [
+                "Historia de México",
+                "Historia Universal",
+                "Cultura y civilizaciones",
+                "Eventos históricos importantes"
+            ],
+            geografia: [
+                "Geografía física de México",
+                "Geografía mundial",
+                "Mapas y cartografía",
+                "Recursos naturales y humanos"
+            ]
+        };
---
+++ data/temario_completo.js
@@ -0,0 +1,26 @@
+const topicsData = {
+            matematicas: [
+                "Números enteros y fracciones",
+                "Álgebra básica",
+                "Geometría plana",
+                "Problemas de razonamiento lógico"
+            ],
+            espanol: [
+                "Ortografía y puntuación",
+                "Comprensión lectora",
+                "Redacción de textos",
+                "Análisis de literatura"
+            ],
+            ciencias: [
+                "Cuerpo humano y biología",
+                "Física básica: fuerza y movimiento",
+                "Química elemental",
+                "Medio ambiente"
+            ],
+            historia: [
+                "Historia de México",
+                "Historia Universal",
+                "Cultura y civilizaciones",
+                "Eventos históricos importantes"
+            ]
+        };
